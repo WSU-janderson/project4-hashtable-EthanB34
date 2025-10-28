156cc2ac2bb8678100411e96e38d2d199cffcf71
EthanB34 <barnes.228@wright.edu>
2025-10-28 19:04:56 -0400

    added function templates
    Added the required function template definitions into .h, declarations will be filled in HashTable.cpp.

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,3 +1,42 @@
 /**
  * HashTable.cpp
  */
+#include "HashTable.h"
+
+HashTable::HashTable(size_t initCapacity) {
+
+}
+
+bool HashTable::insert(std::string key, size_t value) {
+return false;
+}
+
+bool HashTable::remove(std::string key) {
+return false;
+}
+bool HashTable::contains(const std::string& key) const {
+return false;
+}
+
+std::optional<size_t> HashTable::get(const std::string& key) const {
+
+}
+size_t& HashTable::operator[](const std::string& key) {
+
+}
+
+std::vector<std::string> HashTable::keys() const {
+    return{};
+
+}
+
+double HashTable::alpha() const {
+return 0.0;
+}
+size_t HashTable::capacity() const {
+return 0;
+}
+
+size_t HashTable::size() const {
+return 0;
+}
--- HashTable.h
+++ HashTable.h
@@ -1,3 +1,37 @@
 /**
  * HashTable.h
  */
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+#include <optional>
+#include <string>
+#include <vector>
+
+class HashTable {
+public:
+    HashTable(size_t initCapacity = 8);
+
+    bool insert(std::string key, size_t value);
+
+    bool remove(std::string key);
+
+    bool contains(const std::string& key) const;
+
+    std::optional<size_t> get(const std::string& key) const;
+
+    size_t& operator[](const std::string& key);
+
+    std::vector<std::string> keys() const;
+
+    double alpha() const;
+
+    size_t capacity() const;
+    size_t size() const;
+
+
+
+
+
+
+};
+#endif // HASHTABLE_H
